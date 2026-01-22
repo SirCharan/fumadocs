9a6c8751201f776c0f03a2ccbb4c25ea8b04eb72
Charandeep Kapoor <charandeepkapoor3@gmail.com>
2026-01-22 15:26:06 +0530

    pricing without images

--- timelock/next.config.mjs
+++ timelock/next.config.mjs
@@ -5,6 +5,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  serverComponentsExternalPackages: ['remark-math', 'rehype-katex'],
   async rewrites() {
     return [
       {
